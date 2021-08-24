bff1d0d4a3ea09768136f95f832fb7ee597c55a3
spenserTac <spenserdt@gmail.com>
2021-08-24 19:56:23 -0400

    Exercise 1 is done.

--- src/main/java/Exercise_1.java
+++ src/main/java/Exercise_1.java
@@ -1,5 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Spenser Tacinelli
+ */
+
+import java.util.Scanner;
+
 public class Exercise_1{
     public static void main(String[] args){
-        System.out.println("Testing Testing Testing111");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + ", nice to meet you!");
     }
 }
