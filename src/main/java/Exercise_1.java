23d73f8b084569588dc68b1f83a8fab709ae9efb
spenserTac <spenserdt@gmail.com>
2021-08-24 21:26:30 -0400

    Changed println -> print. To keep input on same line.

--- src/main/java/Exercise_1.java
+++ src/main/java/Exercise_1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Exercise_1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
+        System.out.print("What is your name? ");
 
         String name = scanner.nextLine();
 
